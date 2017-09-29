8c002b1832f993c132ee3bd9150292c85bdb1c8e
Shadow5523 <moonstar125y@gmail.com>
2017-09-29 19:27:31 +0900

    shift input support

---
+++ version_6/include/keyboard.h
@@ -0,0 +1,46 @@
+#ifndef _KEYBOARD_H_
+#define _KEYBOARD_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define SCAN_CODE_SET1 0x01
+#define SCAN_CODE_SET2 0x02
+#define SCAN_CODE_SET3 0x03
+
+#define TYPEMATICDELAY_SET01 0x01
+#define TYPEMATICDELAY_SET02 0x02
+#define TYPEMATICDELEY_SET11 0x0B
+
+#define PORTMAP_KEYBOARD1 0x60
+#define PORTMAP_KEYBOARD2 0x61
+
+#define L_SHIFT 0x2a
+#define R_SHIFT 0x36
+
+typedef struct{
+  uint8_t pdata[128];
+  size_t write;
+  size_t read;
+  size_t len;
+}key_buf;
+
+//以下の構造体を追加する
+typedef struct{
+  bool shift_enable;
+  bool relese_enable;
+}key_stat;
+
+key_stat ks;     //追加
+
+void key_init(void);
+uint8_t ps2_kerboard_init(void);
+void keyboard_input_int(uint8_t);
+uint8_t getscode(void);
+uint8_t getchar(void);
+uint8_t getscodeset(void);
+void change_codeset(uint8_t);
+void change_trate_delay(uint8_t); 
+
+#endif _KEYBOARD_H_
---
+++ version_6/keyboard.c
@@ -0,0 +1,122 @@
+#include "include/keyboard.h"
+#include "include/inb_outb.h"
+
+key_buf kb;
+
+void key_init(void){
+  change_trate_delay(TYPEMATICDELEY_SET11);
+  if (ps2_kerboard_init() == 0) {
+    terminal_writestring("PS/2 Keyboard init OK\n");
+  }
+  ks.shift_enable = false;
+  ks.relese_enable = false;
+}
+
+
+uint8_t ps2_kerboard_init(void){
+  change_codeset(SCAN_CODE_SET2);
+  uint8_t scodeset = getscodeset();
+  if (scodeset == 0x43) {
+    terminal_writestring("Scan code set 1\n");
+  } else if (scodeset == 0x41) {
+    terminal_writestring("Scan code set 2\n");
+  } else if (scodeset == 0x3f) {
+    terminal_writestring("Scan code set 3\n");
+  } else {
+    terminal_writestring("Unknown scan code set\n");
+    return 1;
+  }
+  outb(0x60, 0xFA);
+  return 0;
+
+}
+
+
+void keyboard_input_int(uint8_t scan_code){
+  uint8_t us_keytable_set2[0x80] = {
+    '0', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '0',
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+    '\'', '`', '0', '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+    'm', ',', '.', '/', '0', '0', '0', ' ', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0'};
+
+  uint8_t us_keytable_set2S[0x80] = {
+    '0', '0', '!', '@', '#', '$', '%', '^', '&', '*',
+    '(', ')', '_', '+', '\b', '\t', 'Q', 'W', 'E', 'R',
+    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', '0',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',
+    '"', '~', '0', '|', 'Z', 'X', 'C', 'V', 'B', 'N',
+    'M', '<', '>', '?', '0', '0', '0', ' ', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0'};
+
+  if (scan_code <= 0x80) {
+    if (kb.len < 128) {
+      if (scan_code == L_SHIFT) {
+	ks.shift_enable = true;
+      } else {
+	if (ks.shift_enable) {
+	  kb.pdata[kb.write++] = us_keytable_set2S[scan_code];
+	} else {
+	  kb.pdata[kb.write++] = us_keytable_set2[scan_code];
+	}
+	++kb.len;
+	if (kb.write == 128) { kb.write = 0; }
+      }
+    }
+  } else {
+    scan_code -= 0x80;
+    if (scan_code == L_SHIFT || scan_code == R_SHIFT) {
+      ks.shift_enable = false;
+    }
+  }
+}
+
+
+uint8_t getscodeset(void){
+  outb(PORTMAP_KEYBOARD1, 0xf0);
+  outb(PORTMAP_KEYBOARD1, 0x00);
+  return getscode();
+}
+
+
+uint8_t getscode(void){
+  uint8_t c = 0;
+  do {
+    if (inb(PORTMAP_KEYBOARD1) != c) {
+      c = inb(PORTMAP_KEYBOARD1);
+      if (c > 0) return c;
+    }
+  } while (1);
+}
+
+
+uint8_t getchar(void){
+  return getscode();
+}
+
+
+void change_codeset(uint8_t set){
+  outb(PORTMAP_KEYBOARD1, 0xf0);
+  outb(PORTMAP_KEYBOARD1, set);
+}
+
+
+void change_trate_delay(uint8_t set){
+  outb(PORTMAP_KEYBOARD1, 0xf3);
+  outb(PORTMAP_KEYBOARD1, set);
+
+}
